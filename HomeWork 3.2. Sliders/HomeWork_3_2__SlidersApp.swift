07a10aaca937002f8b73789650188ceb81c828ec
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2022-01-24 14:03:56 +0300

    Initial Commit

---
+++ HomeWork 3.2. Sliders/HomeWork_3_2__SlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_3_2__SlidersApp.swift
+//  HomeWork 3.2. Sliders
+//
+//  Created by Ирина on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_3_2__SlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
